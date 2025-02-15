ce93658a4f231de264e5f6c31c517519bbbd8b0d
Simão Lopes <simaolopes98@gmail.com>
2025-02-15 20:20:12 +0000

    Build pass

--- ASM-prj/src/main.cpp
+++ ASM-prj/src/main.cpp
@@ -1,12 +1,26 @@
 // main.c
 
+// defines
 #include <Arduino.h>
 
-void setup() {
+extern "C"
+{
+  void start();
+  void led(byte);
+}
 
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("Setup");
+  start();
+  Serial.println("End of Setup");
 }
 
 void loop() 
 {
-
+  led(1);
+  delay(500);
+  led(0);
+  delay(500);
 }
--- ASM-prj/src/main.s
+++ ASM-prj/src/main.s
@@ -1,1 +1,30 @@
 ; main.s
+
+; define registers
+.equ DDRB, 0x24
+.equ PORTB, 0x25
+
+; global function prototypes
+.global start
+.global led
+
+start:
+    LDI R16, (1 << 4)   ; Load bit mask for PB4 (D12) into R16
+    IN R17, DDRB        ; Read current DDRB state
+    OR R17, R16         ; Set bit 4 (PB4) as output
+    OUT DDRB, R17       ; Write back to DDRB
+    RET                 ; Return to setup()
+
+led:
+    CPI R24, 0x00       ; Compare R24 with 0
+    BREQ ledOFF         ; Branch to ledOFF if R24 == 0
+    IN R17, PORTB       ; Read current PORTB state
+    ORI R17, (1 << 4)   ; Set bit 4 (PB4) to HIGH
+    OUT PORTB, R17      ; Write back to PORTB
+    RET                 ; Return to loop()
+
+ledOFF:
+    IN R17, PORTB       ; Read current PORTB state
+    ANDI R17, ~(1 << 4) ; Clear bit 4 (PB4) to LOW
+    OUT PORTB, R17      ; Write back to PORTB
+    RET                 ; Return to loop()
