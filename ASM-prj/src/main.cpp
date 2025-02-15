5f679aac5379f78ad1733f6a661d284e613df5ec
Simão Lopes <simaolopes98@gmail.com>
2025-02-15 21:15:52 +0000

    Working Blink

--- ASM-prj/src/main.cpp
+++ ASM-prj/src/main.cpp
@@ -2,11 +2,13 @@
 
 // defines
 #include <Arduino.h>
+#include <inttypes.h>
 
 extern "C"
 {
   void start();
   void led(byte);
+  volatile uint8_t ledVal = 1;
 }
 
 void setup() 
@@ -14,13 +16,16 @@ void setup()
   Serial.begin(9600);
   Serial.println("Setup");
   start();
+  delay(1000);
   Serial.println("End of Setup");
 }
 
 void loop() 
 {
   led(1);
+  Serial.println(ledVal);
   delay(500);
   led(0);
+  Serial.println(ledVal);
   delay(500);
 }
--- ASM-prj/src/main.s
+++ ASM-prj/src/main.s
@@ -1,30 +1,24 @@
 ; main.s
 
 ; define registers
-.equ DDRB, 0x24
-.equ PORTB, 0x25
+#define __SFR_OFFSET 0x00
+#include "avr/io.h"
 
 ; global function prototypes
 .global start
 .global led
+.global ledVal
 
 start:
-    LDI R16, (1 << 4)   ; Load bit mask for PB4 (D12) into R16
-    IN R17, DDRB        ; Read current DDRB state
-    OR R17, R16         ; Set bit 4 (PB4) as output
-    OUT DDRB, R17       ; Write back to DDRB
-    RET                 ; Return to setup()
+    SBI DDRB, 4         ;set pin (PB5) D12 as output
+    RET                 ;return
 
 led:
-    CPI R24, 0x00       ; Compare R24 with 0
-    BREQ ledOFF         ; Branch to ledOFF if R24 == 0
-    IN R17, PORTB       ; Read current PORTB state
-    ORI R17, (1 << 4)   ; Set bit 4 (PB4) to HIGH
-    OUT PORTB, R17      ; Write back to PORTB
-    RET                 ; Return to loop()
+    CPI R24, 0x00       ;compare the value passed with 0x00
+    BREQ ledOff         ;jump if equal to routine ledOff
+    SBI PORTB, 4        ;set pin (PB5) D12 to HIGH
+    RET
 
-ledOFF:
-    IN R17, PORTB       ; Read current PORTB state
-    ANDI R17, ~(1 << 4) ; Clear bit 4 (PB4) to LOW
-    OUT PORTB, R17      ; Write back to PORTB
-    RET                 ; Return to loop()
+ledOff:
+    CBI PORTB, 4        ;clear pin (PB5) D12 to LOW
+    RET
