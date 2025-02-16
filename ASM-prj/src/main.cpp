e60622d6f1f8fb2f9e7f7cf6ded294706fa6ae22
Simão Lopes <simaolopes98@gmail.com>
2025-02-16 18:05:36 +0000

    Empty master

--- ASM-prj/src/main.cpp
+++ ASM-prj/src/main.cpp
@@ -2,29 +2,18 @@
 
 // defines
 #include <Arduino.h>
-#include <inttypes.h>
 
 extern "C"
 {
-  void start();
-  void led(byte);
-  volatile uint8_t ledVal;
+
 }
 
 void setup() 
 {
-  Serial.begin(9600);
-  Serial.println("Setup");
-  start();
-  Serial.println("End of Setup");
+
 }
 
 void loop() 
 {
-  led(1);
-  Serial.println(ledVal, BIN);
-  delay(500);
-  led(0);
-  Serial.println(ledVal, BIN);
-  delay(500);
+
 }
